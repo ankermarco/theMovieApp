ae502ea1ed8f260d1ec5e78352d8c328a6c07920
Ke.Ma <ke.ma@progressive-tech.co.uk>
2020-05-05 19:22:53 +0100

    start xcode project

---
+++ theMovieApp/theMovieApp/MovieItem.swift
@@ -0,0 +1,21 @@
+//
+//  MovieItem.swift
+//  theMovieApp
+//
+//  Created by Ke Ma on 05/05/2020.
+//  Copyright © 2020 kma. All rights reserved.
+//
+
+import Foundation
+
+struct MovieItem {
+    let imdb_id: String
+    let genres: String
+    let original_language: String
+    let original_title: String
+    let poster_path: String
+    let release_date: String
+    let title: String
+    let status: String
+    let overview: String
+}
